2d51c9d8471d71746f346181edc365ba2031776d
Chongo Bong <bongochong@users.noreply.github.com>
2019-03-11 22:39:35 -0600

    Add files via upload

---
+++ IPBlockUpdaterP2P/update-bt-bl-withPrompts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo -n "Enter a directory path for this program to operate in: "
+read file_dir
+cd "$file_dir"
+rm level1BT.7z
+rm bt_level1.p2p
+rm level2BT.7z
+rm bt_level2.p2p
+rm combined.txt
+rm combined-final.p2p
+echo "Changed working directory to "$file_dir" and cleaned up old data. Now downloading lists."
+wget -nv -O level1BT.7z "http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw&fileformat=p2p&archiveformat=7z"
+wget -nv -O level2BT.7z "http://list.iblocklist.com/?list=gyisgnzbhppbvsphucsw&fileformat=p2p&archiveformat=7z"
+echo "Downloaded blocklists."
+7z e level1BT.7z
+7z e level2BT.7z
+echo "Extracted blocklists from archives."
+find . -name 'ydxerpxkpcfqjaybcssw.txt' -exec mv {} bt_level1.p2p \;
+find . -name 'gyisgnzbhppbvsphucsw.txt' -exec mv {} bt_level2.p2p \;
+cat *.p2p > combined.txt
+sleep 5
+sort combined.txt | uniq > combined-final.p2p
+perl -pi -e '$_ = "" if ( $. == 1 );' combined-final.p2p
+echo "Appended proper file extension to blocklists. Combined, sorted, and de-duped. Your BitTorrent blocklist has been updated."
