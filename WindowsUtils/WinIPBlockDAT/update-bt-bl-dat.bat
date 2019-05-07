5a3d95ccb7d44e0cbcd1e465bcd4569dbbd62885
Chongo Bong <bongochong@users.noreply.github.com>
2019-05-07 14:18:39 -0600

    First Commit of Windows BitTorrent BlockList Tools

---
+++ WindowsUtils/WinIPBlockDAT/update-bt-bl-dat.bat
@@ -0,0 +1,25 @@
+del "combined-final-win.dat"
+cd bin
+rm level1BTdat.7z
+rm bt_level1.dat
+rm level2BTdat.7z
+rm bt_level2.dat
+rm combined-win.txt
+rm combined-win-sort.txt
+wget -nv -O level1BTdat.7z "http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw&fileformat=dat&archiveformat=7z"
+wget -nv -O level2BTdat.7z "http://list.iblocklist.com/?list=gyisgnzbhppbvsphucsw&fileformat=dat&archiveformat=7z"
+7za e level1BTdat.7z
+7za e level2BTdat.7z
+mv ydxerpxkpcfqjaybcssw.txt bt_level1.dat
+mv gyisgnzbhppbvsphucsw.txt bt_level2.dat
+cat *.dat > combined-win.txt
+timeout 5
+sort combined-win.txt > combined-win-sort.txt
+uniq combined-win-sort.txt > combined-final-win.dat
+timeout 3
+sed -i "/^[[:space:]]*$/d" combined-final-win.dat
+timeout 3
+sed -i "/^#/d" combined-final-win.dat
+timeout 3
+mv combined-final-win.dat ..
+pause
---
+++ WindowsUtils/WinIPBlockP2P/update-bt-bl-p2p.bat
@@ -0,0 +1,25 @@
+del "combined-final.p2p"
+cd bin
+rm level1BT.7z
+rm bt_level1.p2p
+rm level2BT.7z
+rm bt_level2.p2p
+rm combined.txt
+rm combined-sort.txt
+wget -nv -O level1BT.7z "http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw&fileformat=p2p&archiveformat=7z"
+wget -nv -O level2BT.7z "http://list.iblocklist.com/?list=gyisgnzbhppbvsphucsw&fileformat=p2p&archiveformat=7z"
+7za e level1BT.7z
+7za e level2BT.7z
+mv ydxerpxkpcfqjaybcssw.txt bt_level1.p2p
+mv gyisgnzbhppbvsphucsw.txt bt_level2.p2p
+cat *.p2p > combined.txt
+timeout 5
+sort combined.txt > combined-sort.txt
+uniq combined-sort.txt > combined-final.p2p
+timeout 3
+sed -i "/^[[:space:]]*$/d" combined-final.p2p
+timeout 3
+sed -i "/^#/d" combined-final.p2p
+timeout 3
+mv combined-final.p2p ..
+pause
