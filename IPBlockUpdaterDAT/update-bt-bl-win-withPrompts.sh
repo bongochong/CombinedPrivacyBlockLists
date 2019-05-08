502f1e2ee5236416119b4a3eeb326b4c9c40ef70
Chongo Bong <bongochong@users.noreply.github.com>
2019-05-07 22:57:57 -0600

    Update update-bt-bl-win-withPrompts.sh

--- IPBlockUpdaterDAT/update-bt-bl-win-withPrompts.sh
+++ IPBlockUpdaterDAT/update-bt-bl-win-withPrompts.sh
@@ -2,12 +2,14 @@
 echo -n "Enter a directory path for this program to operate in: "
 read file_dir
 cd "$file_dir"
+{
 rm level1BTdat.7z
 rm bt_level1.dat
 rm level2BTdat.7z
 rm bt_level2.dat
 rm combined-win.txt
 rm combined-final-win.dat
+} &> /dev/null 2>&1
 echo "Changed working directory to "$file_dir" and cleaned up old data. Now downloading lists."
 wget -nv -O level1BTdat.7z "http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw&fileformat=dat&archiveformat=7z"
 wget -nv -O level2BTdat.7z "http://list.iblocklist.com/?list=gyisgnzbhppbvsphucsw&fileformat=dat&archiveformat=7z"
