30aef83546d62f88f78caba52c33b2b413ded14d
Chongo Bong <bongochong@users.noreply.github.com>
2019-05-08 20:07:37 -0600

    Update update-pac-win-template.bat

--- WindowsUtils/WinPACUpdater/update-pac-win-template.bat
+++ WindowsUtils/WinPACUpdater/update-pac-win-template.bat
@@ -9,4 +9,6 @@ sed -i "s|PROXY 127.0.0.1|PROXY 0.0.0.0:3421|" yoyo.pac
 cp yoyo.pac yoyo.pac.js
 curl -T yoyo.pac ftp://SERVER-AND-DIRECTORY-HERE --user USERNAME-HERE:PASSWORD-HERE
 curl -T yoyo.pac.js ftp://SERVER-AND-DIRECTORY-HERE --user USERNAME-HERE:PASSWORD-HERE
+@echo off
+echo Done!
 pause
