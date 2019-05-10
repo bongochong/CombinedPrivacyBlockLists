eea218bf65cd5010c8950190aed23ea820653af6
bongochong <arbonarbot@gmail.com>
2019-05-10 16:08:26 -0600

    updated windows PAC template

--- WindowsUtils/WinPACUpdater/update-pac-win-template.bat
+++ WindowsUtils/WinPACUpdater/update-pac-win-template.bat
@@ -1,15 +1,35 @@
 cd %~dp0
 @echo off
+del "pac-done.js"
 cd bin
-rm yoyo.pac
-rm yoyo.pac.js
+rm pac-*
+rm *.pac
 @echo on
-wget -nv -O yoyo.pac.txt "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=proxyautoconfig&mimetype=plaintext"
-mv yoyo.pac.txt yoyo.pac
-sed -i "s|PROXY 127.0.0.1|PROXY 0.0.0.0:3421|" yoyo.pac
-cp yoyo.pac yoyo.pac.js
-curl -T yoyo.pac ftp://SERVER-AND-DIRECTORY-HERE --user USERNAME-HERE:PASSWORD-HERE
-curl -T yoyo.pac.js ftp://SERVER-AND-DIRECTORY-HERE --user USERNAME-HERE:PASSWORD-HERE
+wget -nv -O 1.pac "https://ssl.bblck.me/blacklists/domain-list.txt"
+wget -nv -O 2.pac "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
+wget -nv -O 3.pac "https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt"
+wget -nv -O 4.pac "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt"
+wget -nv -O 5.pac "https://dshield.org/feeds/suspiciousdomains_Medium.txt"
+wget -nv -O 6.pac "https://dshield.org/feeds/suspiciousdomains_High.txt"
+wget -nv -O 7.pac "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml&showintro=0&mimetype=plaintext"
+cat *.pac > pac-comb.txt
+sed -i "s/#.*$//" pac-comb.txt
+sed -i "/^$/d" pac-comb.txt
+sort pac-comb.txt > pac-sort.txt
+uniq pac-sort.txt > pac-uniq.txt
+head -c -1 pac-uniq.txt > pac-pre.txt
+cp pac-pre.txt pac-pre2.txt
+sed -i "s/^/*./" pac-pre.txt
+cat pac-pre.txt pac-pre2.txt > pac-wew.txt
+head -c -1 pac-wew.txt > pac-lad.txt 
+sed -i "s/^/shExpMatch(host, '/" pac-lad.txt
+sed -i "s/$/') ||/" pac-lad.txt
+sed "2r pac-lad.txt" < pactemplate.txt > pac-done.txt
+cp pac-done.txt pac-done.js
+curl -T pac-done.js ftp://SERVER-AND-DIRECTORY-HERE --user USERNAME-HERE:PASSWORD-HERE
+cp pac-done.txt ..
+cd ..
+rename pac-done.txt pac-done.js
 @echo off
 echo Done!
 pause
