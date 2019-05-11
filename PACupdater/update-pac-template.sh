31e76c97bf69c7242667c3c0225399d602237c8a
bongochong <arbonarbot@gmail.com>
2019-05-10 18:25:24 -0600

    Updated PAC file template for LiGnux. Much better now.

--- PACupdater/update-pac-template.sh
+++ PACupdater/update-pac-template.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
-cd YOUR-DIRECTORY-HERE
+echo -n "Enter a directory path for this program to operate in: "
+read file_dir
+cd "$file_dir"
 {
-rm yoyo.pac
-rm yoyo.pac.js
+rm pac-*
+rm *.pac
 } &> /dev/null 2>&1
-echo "Changed working directory to YOUR-DIRECTORY-HERE"
-wget -nv -O YOUR-DIRECTORY-HERE/yoyo.pac.txt "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=proxyautoconfig&mimetype=plaintext"
-echo "Downloaded PAC file"
-mv yoyo.pac.txt yoyo.pac
-echo "Properly saved PAC file"
-sed -i "s|PROXY 127.0.0.1|PROXY 0.0.0.0:3421|" yoyo.pac
-perl -i -pe 'chomp if eof' yoyo.pac
-echo "Properly formatted PAC file"
-cp yoyo.pac yoyo.pac.js
-curl -T yoyo.pac ftp://SERVER-AND-DIRECTORY-HERE --user USERNAME-HERE:PASSWORD-HERE
-curl -T yoyo.pac.js ftp://SERVER-AND-DIRECTORY-HERE --user USERNAME-HERE:PASSWORD-HERE
+echo "Changed working directory to "$file_dir" and cleaned up old data. Now downloading new lists."
+wget -nv -O 1.pac "https://ssl.bblck.me/blacklists/domain-list.txt"
+wget -nv -O 2.pac "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
+wget -nv -O 3.pac "https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt"
+wget -nv -O 4.pac "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt"
+wget -nv -O 5.pac "https://dshield.org/feeds/suspiciousdomains_Medium.txt"
+wget -nv -O 6.pac "https://dshield.org/feeds/suspiciousdomains_High.txt"
+wget -nv -O 7.pac "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml&showintro=0&mimetype=plaintext"
+echo "Lists Downloaded."
+cat *.pac > pac-comb.txt
+sed -i "s/#.*$//" pac-comb.txt
+sed -i "/^$/d" pac-comb.txt
+sort pac-comb.txt > pac-sort.txt
+uniq pac-sort.txt > pac-uniq.txt
+head -c -1 pac-uniq.txt > pac-pre.txt
+cp pac-pre.txt pac-pre2.txt
+sed -i "s/^/*./" pac-pre.txt
+cat pac-pre.txt pac-pre2.txt > pac-wew.txt
+head -c -1 pac-wew.txt > pac-lad.txt 
+sed -i "s/^/shExpMatch(host, '/" pac-lad.txt
+sed -i "s/$/') ||/" pac-lad.txt
+sed "2r pac-lad.txt" < pactemplate.txt > pac-done.txt
+cp pac-done.txt pac-done.js
+echo "Properly merged and formatted block lists into new PAC file."
+curl -T pac-done.js ftp://SERVER-AND-DIRECTORY-HERE --user USERNAME-HERE:PASSWORD-HERE
 echo "Successfully uploaded PAC to remote servers. We're done!"
