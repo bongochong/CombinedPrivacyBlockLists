187a10b0b3a789328d633e42f7ab51b6abda688e
bongochong <bongochong@github.com>
2019-09-11 22:05:00 -0600

    removed unnecessary operations from host updater scripts

--- HostsUpdater/update-hosts-withPrompts.sh
+++ HostsUpdater/update-hosts-withPrompts.sh
@@ -9,42 +9,24 @@ rm *.hosts
 rm uniq-hosts-final.pre
 rm newhosts.txt
 echo "Changed working directory to "$file_dir" and removed old files."
-wget -nv -O hosts.1.txt "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"
+wget -nv -O hosts.1 "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"
 echo "Downloaded hosts list 1"
-mv hosts.1.txt hosts.1
-echo "Properly saved hosts list 1"
-wget -nv -O hosts.2.txt "http://winhelp2002.mvps.org/hosts.txt"
+wget -nv -O hosts.2 "http://winhelp2002.mvps.org/hosts.txt"
 echo "Downloaded hosts list 2"
-mv hosts.2.txt hosts.2
-echo "Properly saved hosts list 2"
-wget -nv -O hosts.3.txt "http://www.malwaredomainlist.com/hostslist/hosts.txt"
+wget -nv -O hosts.3 "http://www.malwaredomainlist.com/hostslist/hosts.txt"
 echo "Downloaded hosts list 3"
-mv hosts.3.txt hosts.3
-echo "Properly saved hosts list 3"
-wget -nv -O hosts.4.txt "http://hosts-file.net/ad_servers.txt"
+wget -nv -O hosts.4 "http://hosts-file.net/ad_servers.txt"
 echo "Downloaded hosts list 4"
-mv hosts.4.txt hosts.4
-echo "Properly saved hosts list 4"
-wget -nv -O hosts.5.txt "http://hosts-file.net/exp.txt"
+wget -nv -O hosts.5 "http://hosts-file.net/exp.txt"
 echo "Downloaded hosts list 5"
-mv hosts.5.txt hosts.5
-echo "Properly saved hosts list 5"
-wget -nv -O hosts.6.txt "https://adaway.org/hosts.txt"
+wget -nv -O hosts.6 "https://adaway.org/hosts.txt"
 echo "Downloaded hosts list 6"
-mv hosts.6.txt hosts.6
-echo "Properly saved hosts list 6"
-wget -nv -O hosts.7.txt "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/WindowsTelemetryBlockSupplements/SBBTYZ-IPv4.txt"
+wget -nv -O hosts.7 "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/WindowsTelemetryBlockSupplements/SBBTYZ-IPv4.txt"
 echo "Downloaded hosts list 7"
-mv hosts.7.txt hosts.7
-echo "Properly saved hosts list 7"
-# wget -nv -O hosts.8.txt "https://github.com/bongochong/CombinedPrivacyBlockLists/raw/master/NoFormatting/ChefKoch-NSA-BlockList-IPv4.txt"
-# echo "Downloaded hosts list 8"
-# mv hosts.8.txt hosts.8
-# echo "Properly saved hosts list 8"
-wget -nv -O hosts.9.txt "https://1hos.cf/mini/"
-echo "Downloaded hosts list 9"
-mv hosts.9.txt hosts.9
-echo "Properly saved hosts list 9"
+wget -nv -O hosts.8 "https://1hos.cf/mini/"
+echo "Downloaded hosts list 8"
+# wget -nv -O hosts.9 "https://github.com/bongochong/CombinedPrivacyBlockLists/raw/master/NoFormatting/ChefKoch-NSA-BlockList-IPv4.txt"
+# echo "Downloaded hosts list 9"
 cat hosts.* > hosts-cat.final
 pcregrep -v -f hostpatterns.dat hosts-cat.final > hosts-pre.final
 sort hosts-pre.final > sorted-hosts.final
