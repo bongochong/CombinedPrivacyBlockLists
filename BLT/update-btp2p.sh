a30aa9afa2f5bf8e409c96bd2eca60101d1a0a04
bongochong <arbonarbot@gmail.com>
2020-05-25 04:14:12 -0600

    further concatenation work on P2P-list scripts

--- BLT/update-btp2p.sh
+++ BLT/update-btp2p.sh
@@ -5,7 +5,7 @@
 echo "Cleaning up & Fetching P2P lists..."
 mkdir -p ~/BLT/btp2p
 cd ~/BLT/btp2p
-rm -f level1BT.7z bt_level1.p2p level2BT.7z bt_level2.p2p combined.txt combined-final.p2p
+rm -f level1BT.7z bt_level1.p2p level2BT.7z bt_level2.p2p combined-final.p2p
 wget -nv -O level1BT.7z "http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw&fileformat=p2p&archiveformat=7z"
 wget -nv -O level2BT.7z "http://list.iblocklist.com/?list=gyisgnzbhppbvsphucsw&fileformat=p2p&archiveformat=7z"
 echo "Downloaded blocklists."
@@ -16,13 +16,12 @@ sleep 1
 echo "Extracted blocklists from archives."
 find . -name 'ydxerpxkpcfqjaybcssw.txt' -exec mv {} bt_level1.p2p \;
 find . -name 'gyisgnzbhppbvsphucsw.txt' -exec mv {} bt_level2.p2p \;
-cat *.p2p | sed "s/amp;//g" > combined.txt
-sort combined.txt | uniq -i > combined-final.p2p
+cat *.p2p | sed "s/amp;//g" | sort | uniq -i > combined-final.p2p
 sleep 1
 perl -pi -e '$_ = "" if ( $. == 1 );' combined-final.p2p
 sed -i -e "/^#/d" -e "/^[[:space:]]*$/d" combined-final.p2p
 perl -i -pe 'chomp if eof' combined-final.p2p
 echo "Appended proper file extension to blocklists. Combined, sorted, and de-duped. Your BitTorrent blocklist has been updated."
-rm -f level1BT.7z bt_level1.p2p level2BT.7z bt_level2.p2p combined.txt
+rm -f level1BT.7z bt_level1.p2p level2BT.7z bt_level2.p2p
 xdg-open ~/BLT/btp2p
 exit
