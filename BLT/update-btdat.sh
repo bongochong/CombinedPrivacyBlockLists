8ffd01294d2e28dc50909ed6e22288f898d68da0
bongochong <arbonarbot@gmail.com>
2021-05-27 16:32:35 -0600

    updated p2p IP block lists, associated scripts in BLT, and readme

--- BLT/update-btdat.sh
+++ BLT/update-btdat.sh
@@ -8,24 +8,24 @@
 echo "Cleaning up & Fetching DAT lists..."
 mkdir -p ~/BLT/btdat
 cd ~/BLT/btdat
-rm -f level1BTdat.7z bt_level1.dat level2BTdat.7z bt_level2.dat combined-final-win.dat
+rm -f level1BTdat.7z bt_level1.dat bpeersBTdat.7z bt_bpeers.dat combined-final-win.dat
 wget -nv -O level1BTdat.7z "http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw&fileformat=dat&archiveformat=7z"
-wget -nv -O level2BTdat.7z "http://list.iblocklist.com/?list=gyisgnzbhppbvsphucsw&fileformat=dat&archiveformat=7z"
+wget -nv -O bpeersBTdat.7z "http://list.iblocklist.com/?list=cwworuawihqvocglcoss&fileformat=dat&archiveformat=7z"
 echo "Downloaded blocklists."
 7z e level1BTdat.7z
 sleep 1
-7z e level2BTdat.7z
+7z e bpeersBTdat.7z
 sleep 1
 echo "Extracted blocklists from archives."
 find . -name 'ydxerpxkpcfqjaybcssw.txt' -exec mv {} bt_level1.dat \;
-find . -name 'gyisgnzbhppbvsphucsw.txt' -exec mv {} bt_level2.dat \;
+find . -name 'cwworuawihqvocglcoss.txt' -exec mv {} bt_bpeers.dat \;
 cat *.dat | sed "s/amp;//g" | sed "s/\&//g" | tr -d "\200-\377" | sort -f | uniq -i > combined-final-win.dat
 sleep 1
 perl -pi -e '$_ = "" if ( $. == 1 );' combined-final-win.dat
 sed -i -e "/^#/d" -e "/^[[:space:]]*$/d" combined-final-win.dat
 perl -i -pe 'chomp if eof' combined-final-win.dat
 echo "Appended proper file extension to blocklists. Combined, sorted, and de-duped. Your BitTorrent blocklist has been updated."
-rm -f level1BTdat.7z bt_level1.dat level2BTdat.7z bt_level2.dat
+rm -f level1BTdat.7z bt_level1.dat bpeersBTdat.7z bt_bpeers.dat
 echo "~---_---_---~"
 ls -lh ~/BLT/btdat
 echo "~---_---_---~"
--- BLT/update-btp2p.sh
+++ BLT/update-btp2p.sh
@@ -8,24 +8,24 @@
 echo "Cleaning up & Fetching P2P lists..."
 mkdir -p ~/BLT/btp2p
 cd ~/BLT/btp2p
-rm -f level1BT.7z bt_level1.p2p level2BT.7z bt_level2.p2p combined-final.p2p
+rm -f level1BT.7z bt_level1.p2p bpeersBT.7z bt_bpeers.p2p combined-final.p2p
 wget -nv -O level1BT.7z "http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw&fileformat=p2p&archiveformat=7z"
-wget -nv -O level2BT.7z "http://list.iblocklist.com/?list=gyisgnzbhppbvsphucsw&fileformat=p2p&archiveformat=7z"
+wget -nv -O bpeersBT.7z "http://list.iblocklist.com/?list=cwworuawihqvocglcoss&fileformat=p2p&archiveformat=7z"
 echo "Downloaded blocklists."
 7z e level1BT.7z
 sleep 1
-7z e level2BT.7z
+7z e bpeersBT.7z
 sleep 1
 echo "Extracted blocklists from archives."
 find . -name 'ydxerpxkpcfqjaybcssw.txt' -exec mv {} bt_level1.p2p \;
-find . -name 'gyisgnzbhppbvsphucsw.txt' -exec mv {} bt_level2.p2p \;
+find . -name 'cwworuawihqvocglcoss.txt' -exec mv {} bt_bpeers.p2p \;
 cat *.p2p | sed "s/amp;//g" | sed "s/\&//g" | tr -d "\200-\377" | sort -f | uniq -i > combined-final.p2p
 sleep 1
 perl -pi -e '$_ = "" if ( $. == 1 );' combined-final.p2p
 sed -i -e "/^#/d" -e "/^[[:space:]]*$/d" combined-final.p2p
 perl -i -pe 'chomp if eof' combined-final.p2p
 echo "Appended proper file extension to blocklists. Combined, sorted, and de-duped. Your BitTorrent blocklist has been updated."
-rm -f level1BT.7z bt_level1.p2p level2BT.7z bt_level2.p2p
+rm -f level1BT.7z bt_level1.p2p bpeersBT.7z bt_bpeers.p2p
 echo "~---_---_---~"
 ls -lh ~/BLT/btp2p
 echo "~---_---_---~"
