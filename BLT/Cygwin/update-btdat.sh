db97ae7e9c4fce8d8901effffb20a2a3c6b78525
bongochong <arbonarbot@gmail.com>
2021-07-13 19:46:22 -0600

    re-commit of BLT scripts for Cygwin

---
+++ BLT/Cygwin/update-btdat.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#Cygwin Block List Tools (http://shorl.com/fovogretopiga)
+#From the maintainer of Combined Privacy Block Lists (https://github.com/bongochong/CombinedPrivacyBlockLists)
+#License: CPAL-1.0 (https://github.com/bongochong/CombinedPrivacyBlockLists/blob/master/LICENSE.md)
+	read -r -p "Are you ready to update your peer-to-peer block list? [y/N] " response
+	if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+	then
+echo "Cleaning up & Fetching DAT lists..."
+mkdir -p ~/BLT/btdat
+cd ~/BLT/btdat
+rm -f level1BTdat.7z bt_level1.dat bpeersBTdat.7z bt_bpeers.dat combined-final-win.dat
+wget -nv -O level1BTdat.7z "http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw&fileformat=dat&archiveformat=7z"
+wget -nv -O bpeersBTdat.7z "http://list.iblocklist.com/?list=cwworuawihqvocglcoss&fileformat=dat&archiveformat=7z"
+echo "Downloaded blocklists."
+7z e level1BTdat.7z
+sleep 1
+7z e bpeersBTdat.7z
+sleep 1
+echo "Extracted blocklists from archives."
+find . -name 'ydxerpxkpcfqjaybcssw.txt' -exec mv {} bt_level1.dat \;
+find . -name 'cwworuawihqvocglcoss.txt' -exec mv {} bt_bpeers.dat \;
+cat *.dat | sed "s/amp;//g" | sed "s/\&//g" | tr -d "\200-\377" | sort -f | uniq -i > combined-final-win.dat
+sleep 1
+perl -pi -e '$_ = "" if ( $. == 1 );' combined-final-win.dat
+sed -i -e "/^#/d" -e "/^[[:space:]]*$/d" combined-final-win.dat
+perl -i -pe 'chomp if eof' combined-final-win.dat
+echo "Appended proper file extension to blocklists. Combined, sorted, and de-duped. Your BitTorrent blocklist has been updated."
+rm -f level1BTdat.7z bt_level1.dat bpeersBTdat.7z bt_bpeers.dat
+echo "~---_---_---~"
+ls -lh ~/BLT/btdat
+echo "~---_---_---~"
+datCount=$(cat ~/BLT/btdat/combined-final-win.dat | wc -l)
+echo "Your current P2P block list contains $datCount unique entries..."
+echo "~---_---_---~"
+exit
+	else
+	echo "Alright, goodbye!"
+	fi
+exit
---
+++ BLT/Cygwin/update-pac.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#Cygwin Block List Tools (http://shorl.com/fovogretopiga)
+#From the maintainer of Combined Privacy Block Lists (https://github.com/bongochong/CombinedPrivacyBlockLists)
+#License: CPAL-1.0 (https://github.com/bongochong/CombinedPrivacyBlockLists/blob/master/LICENSE.md)
+	read -r -p "Are you ready to update your proxy auto-configuration block list? [y/N] " response
+	if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+	then
+echo "Cleaning up & Fetching PAC lists..."
+mkdir -p ~/BLT/pac
+cd ~/BLT/pac
+rm -f pac-* *.pac
+sleep 1
+echo "Changed working directory and cleaned up old data. Now downloading new lists."
+wget -nv -O 1.pac "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/NoFormatting/MD-ID-Fork.txt"
+wget -nv -O 2.pac "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
+wget -nv -O 3.pac "https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt"
+wget -nv -O 4.pac "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt"
+wget -nv -O 5.pac "https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt"
+wget -nv -O 6.pac "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml&showintro=0&mimetype=plaintext"
+wget -nv -O 7.pac "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/ABP2Hosts/piperun-hosts.txt"
+echo "Lists Downloaded. Now parsing..."
+sed -e '$s/$/\n/' -s *.pac | sed "s/#.*$//" | sed "/^$/d" | sed "/^Site$/d" | sed "s/\(.*\)/\L\1/" | pcregrep -f ~/BLT/parsing/tld-filter.dat | sed "s/^127.0.0.1 //g" | sed "s/^0.0.0.0 //g" | sed "s/^::1 //g" | sed "s/^:: //g" | pcregrep -v -f ~/BLT/parsing/pacpatterns.dat | sed "/@/d" | sed "/ /d" | sed "/\[/d" | sed "/\]/d" | sed "/\//d" | sort -f | uniq -i > pac-uniq.txt
+cp pac-uniq.txt pac-pre.txt
+cp pac-pre.txt pac-pre2.txt
+sed -i "s/^/*./" pac-pre.txt
+cat pac-pre.txt pac-pre2.txt > pac-wew.txt
+perl -i -pe 'chomp if eof' pac-wew.txt
+sed "s/^/shExpMatch(host, '/" pac-wew.txt > pac-lad.txt
+sed -i "s/$/') ||/" pac-lad.txt
+sed "2r pac-lad.txt" < ~/BLT/parsing/pactemplate.txt > pac-done.js
+sed -i "1s/^/\/\/ **** $(date -u)\n/" pac-done.js
+perl -i -pe 'chomp if eof' pac-done.js
+echo "Properly merged and formatted block lists into new PAC file."
+# curl -T pac-done.js ftp://SERVER-AND-DIRECTORY-HERE --user USERNAME-HERE:PASSWORD-HERE
+# echo "Successfully uploaded PAC to remote servers. We're done!"
+sleep 1
+rm -f *.pac *.txt
+echo "~---_---_---~"
+ls -lh ~/BLT/pac
+echo "~---_---_---~"
+paCount=$(grep -w "shExpMatch" -c ~/BLT/pac/pac-done.js)
+echo "Your current PAC file contains $paCount unique entries..."
+echo "~---_---_---~"
+exit
+	else
+	echo "Alright, goodbye!"
+	fi
+exit
