e9f0dcf26529d08928143bbc47dbc29e88aa4e55
bongochong <arbonarbot@gmail.com>
2021-07-13 19:11:01 -0600

    initial commit of Cygwin Block List Tools

---
+++ BLT/Cygwin/cpbl-installer.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+#Cygwin Block List Tools (https://github.com/bongochong/CombinedPrivacyBlockLists/tree/master/BLT/Cygwin)
+#From the maintainer of Combined Privacy Block Lists (https://github.com/bongochong/CombinedPrivacyBlockLists)
+#License: CPAL-1.0 (https://github.com/bongochong/CombinedPrivacyBlockLists/blob/master/LICENSE.md)
+echo "..."
+echo -n "Which directory would you like these tools to be installed in under your home folder? "
+read file_dir
+echo "..."
+echo "Creating directories & Downloading block list utilities and documentation..."
+echo "..."
+mkdir -p ~/"$file_dir"/btdat
+mkdir -p ~/"$file_dir"/btp2p
+mkdir -p ~/"$file_dir"/hosts
+mkdir -p ~/"$file_dir"/pac
+mkdir -p ~/"$file_dir"/parsing
+mkdir -p ~/"$file_dir"/readmes
+echo "Downloading documentation and scripts..."
+cd ~/"$file_dir"
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/README.md" -O README.md
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/update-btdat.sh" -O update-btdat.sh
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/update-btp2p.sh" -O update-btp2p.sh
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/update-hosts-dual.sh" -O update-hosts-dual.sh
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/update-hosts.sh" -O update-hosts.sh
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/update-hosts-six.sh" -O update-hosts-six.sh
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/update-pac.sh" -O update-pac.sh
+cd ~/"$file_dir"/parsing
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/parsing/hostpatterns.dat" -O hostpatterns.dat
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/parsing/pacpatterns.dat" -O pacpatterns.dat
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/parsing/tld-filter.dat" -O tld-filter.dat
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/parsing/newhosts-template-dual.txt" -O newhosts-template-dual.txt
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/parsing/newhosts-template.txt" -O newhosts-template.txt
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/parsing/newhosts-template-six.txt" -O newhosts-template-six.txt
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/parsing/pactemplate.txt" -O pactemplate.txt
+cd ~/"$file_dir"/readmes
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/readmes/README-btdat.md" -O README-btdat.md
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/readmes/README-btp2p.md" -O README-btp2p.md
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/readmes/README-hosts.md" -O README-hosts.md
+wget -nv "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/BLT/Cygwin/readmes/README-pac.md" -O README-pac.md
+echo "..."
+echo "Ensuring that scripts are made executable..."
+echo "..."
+cd ~/"$file_dir"
+chmod +x update-btdat.sh update-btp2p.sh update-hosts-dual.sh update-hosts.sh update-hosts-six.sh update-pac.sh
+echo "Ensuring that scripts reflect your install directory..."
+echo "..."
+sed -i "s/BLT/$file_dir/g" update-btdat.sh
+sed -i "s/BLT/$file_dir/g" update-btp2p.sh
+sed -i "s/BLT/$file_dir/g" update-hosts-six.sh
+sed -i "s/BLT/$file_dir/g" update-hosts-dual.sh
+sed -i "s/BLT/$file_dir/g" update-hosts.sh
+sed -i "s/BLT/$file_dir/g" update-pac.sh
+echo -n "What is the letter of your Windows System Drive (if you don't know, then just type C and hit enter)?: "
+read sys_drive
+sed -i "s/C: $sys_drive:/g" update-hosts.sh
+sed -i "s/C: $sys_drive:/g" update-hosts-six.sh
+sed -i "s/C: $sys_drive:/g" update-hosts-dual.sh
+echo "Inserted host name into hosts file templates."
+echo "..."
+read -r -p "Finally, would you like these tools to be automatically aliased in your bash configuration file, for easy launching? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+echo -e "\nalias blthosts='~/$file_dir/update-hosts.sh'" >> ~/.bashrc
+echo "alias blthosts6='~/$file_dir/update-hosts-six.sh'" >> ~/.bashrc
+echo "alias blthosts2='~/$file_dir/update-hosts-dual.sh'" >> ~/.bashrc
+echo "alias bltpac='~/$file_dir/update-pac.sh'" >> ~/.bashrc
+echo "alias bltdat='~/$file_dir/update-btdat.sh'" >> ~/.bashrc
+echo "alias bltp2p='~/$file_dir/update-btp2p.sh'" >> ~/.bashrc
+echo "Done! All BLT aliases begin with the term 'blt', and you may see them by"
+echo " reloading bash, typing 'alias' into your console prompt, then hitting enter."
+echo "..."
+echo "In addition, if you wish to utilize all the scripts"
+echo "Make sure that you have the following packages installed:"
+echo "perl wget pcre curl p7zip libidn2 libidn2_0 or their equivalents"
+echo "Enjoy!"
+echo "..."
+exit
+else
+echo "..."
+echo "Done! Run whichever script you like via sh or dot-slash."
+echo "..."
+echo "In addition, if you wish to utilize all the scripts"
+echo "Make sure that you have the following packages installed:"
+echo "perl wget pcre curl p7zip libidn2 libidn2_0 or their equivalents"
+echo "Enjoy!"
+echo "..."
+ls -lh ~/"$file_dir"
+fi
+exit
---
+++ BLT/Cygwin/update-hosts-six.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#Cygwin Block List Tools (http://shorl.com/fovogretopiga)
+#From the maintainer of Combined Privacy Block Lists (https://github.com/bongochong/CombinedPrivacyBlockLists)
+#License: CPAL-1.0 (https://github.com/bongochong/CombinedPrivacyBlockLists/blob/master/LICENSE.md)
+	read -r -p "Are you ready to update your hosts file? [y/N] " response
+	if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+	then
+echo "Cleaning up & Fetching hosts lists..."
+mkdir -p ~/BLTestWin/hosts
+cd ~/BLTestWin/hosts
+rm -f hosts.* *.final *.hosts newhosts.txt
+sleep 1
+wget -nv -O hosts.1 "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"
+echo "Downloaded hosts list 1"
+wget -nv -O hosts.2 "https://winhelp2002.mvps.org/hosts.txt"
+echo "Downloaded hosts list 2"
+wget -nv -O hosts.3 "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/NoFormatting/MD-ID-H-Fork.txt"
+echo "Downloaded hosts list 3"
+wget -nv -O hosts.4 "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/ABP2Hosts/adguard_desktop-hosts.txt"
+echo "Downloaded hosts list 4"
+wget -nv -O hosts.5 "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/ABP2Hosts/spam_404-hosts.txt"
+echo "Downloaded hosts list 5"
+wget -nv -O hosts.6 "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/ABP2Hosts/adguard_mobile-hosts.txt"
+echo "Downloaded hosts list 6"
+wget -nv -O hosts.7 "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/WindowsTelemetryBlockSupplements/SBBTYZ-IPv4.txt"
+echo "Downloaded hosts list 7"
+wget -nv -O hosts.8 "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/ABP2Hosts/easylist_desktop-hosts.txt"
+echo "Downloaded hosts list 8"
+wget -nv -O hosts.9 "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/ABP2Hosts/adguard_dns-hosts.txt"
+echo "Downloaded hosts list 9"
+wget -nv -O hosts.10 "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/ABP2Hosts/piperun-hosts.txt"
+echo "Downloaded hosts list 10"
+wget -nv -O hosts.11 "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/ABP2Hosts/disconnect_consolidated.txt"
+echo "Downloaded hosts list 11"
+wget -nv -O hosts.12 "https://gitlab.com/curben/urlhaus-filter/raw/master/urlhaus-filter-hosts-online.txt"
+echo "Downloaded hosts list 12"
+wget -nv -O hosts.13 "https://raw.githubusercontent.com/bongochong/CombinedPrivacyBlockLists/master/NoFormatting/AdditionalSupplementaryHosts.txt"
+echo "Downloaded hosts list 13"
+echo "Parsing data..."
+cat hosts.* > hosts-cat.final
+sort hosts-cat.final | uniq | sed "s/#.*$//" | sed "/[[:space:]]*#/d" | sed "/[[:blank:]]*#/d" | sed "s/\t/ /g" | sed "s/^127.0.0.1/0.0.0.0/g" | sed "s/^::1/0.0.0.0/g" | sed "s/^::/0.0.0.0/g" | sed "s/[[:space:]]*$//" | sed "s/[[:blank:]]*$//" | sed "s/[[:space:]]\+/ /g" | sed "/^0.0.0.0 /! s/^/0.0.0.0 /" | sed "s/\(.*\)/\L\1/" > hosts-pre.final
+pcregrep -v -f ~/BLTestWin/parsing/hostpatterns.dat hosts-pre.final > uniq-hosts.final
+#Routine to check for and convert Unicode IDNs to Punycode
+if [[ $(grep -P -n "[^\x00-\x7F]" uniq-hosts.final) ]]; then
+    echo "Non-ASCII strings found in domains. Converting to Punycode..."
+    grep -P -v "[^\x00-\x7F]" uniq-hosts.final | sed "s/0.0.0.0 //" > uniq-hosts-na.final
+    grep -P "[^\x00-\x7F]" uniq-hosts.final | sed "s/0.0.0.0 //" | idn2 >> uniq-hosts-na.final
+    sed -i "/^0.0.0.0 /! s/^/0.0.0.0 /" uniq-hosts-na.final
+    rm -f uniq-hosts.final && mv uniq-hosts-na.final uniq-hosts.final
+else
+    echo "All domains are valid ASCII strings. Continuing compilation of lists..."
+fi
+#End of Unicode IDN to Punycode conversion routine
+sed -i -e '/\^\document/d' -e '/\^/d' -e '/\*/d' -e '/\?/d' -e '/\//d' -e '/@/d' -e '/!/d' -e '/|/d' -e '/:/d' -e '/~/d' -e '/,/d' -e '/=/d' -e "/\[/d" -e "/\]/d" -e '/\//d' uniq-hosts.final
+sort -f uniq-hosts.final | uniq -i > final-uniq.hosts
+pcregrep -f ~/BLTestWin/parsing/tld-filter.dat final-uniq.hosts > hosts.final
+perl -i -pe 'chomp if eof' hosts.final
+echo "Successfully merged hosts lists!"
+sed -i -e "s/^127.0.0.1/::/g" -e "s/^::1/::/g" -e "s/^0.0.0.0/::/g" -e "/^:: /! s/^/:: /" hosts.final
+sed '35r hosts.final' < ~/BLTestWin/parsing/newhosts-template-six.txt > newhosts.txt
+sed -i "23s|DAYBONGODATEREPLACE|$(date -u)|" newhosts.txt
+perl -i -pe 'chomp if eof' newhosts.txt
+echo "Successfully cleaned up and formatted hosts file! Prompting for password to make backup of and overwrite C:/Windows/System32/Drivers/etc/HOSTS..."
+cp C:/Windows/System32/Drivers/etc/HOSTS hostsbackup.txt
+cygstart --action=runas cp newhosts.txt C:/Windows/System32/Drivers/etc/HOSTS
+sleep 1
+rm -f hosts.* *.final *.hosts
+echo "Your hosts file has been updated!"
+echo "~---_---_---~"
+ls -lh ~/BLTestWin/hosts
+echo "~---_---_---~"
+hostCount=$(grep -w "#" -c -v C:/Windows/System32/Drivers/etc/HOSTS)
+echo "Your current hosts file contains $hostCount unique entries..."
+echo "~---_---_---~"
+exit
+	else
+	echo "Alright, goodbye!"
+	fi
+exit
